5208a198875280323de753547bb30b48a54ed772
techwulf <t.zach.moore@gmail.com>
2021-10-06 19:37:00 -0600

    complete mvp

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,38 +1,38 @@
-/*
-  If `scheme_id` does not exist in the database:
+const db = require('../../data/db-config');
 
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
+const checkSchemeId = async(req, res, next) => {
+  const [schemeExists] = await db('schemes')
+    .where('scheme_id', req.params.scheme_id);
+  if (schemeExists) {
+    next();
+  } else {
+    next({
+      status: 404, 
+      message: `scheme with scheme_id ${req.params.scheme_id} not found`
+    });
   }
-*/
-const checkSchemeId = (req, res, next) => {
-
 }
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
-
+  if (req.body.scheme_name && typeof(req.body.scheme_name) === 'string') {
+    next();
+  } else {
+    next({status:400, message: 'invalid scheme_name'})
+  }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
-
+  if(
+    req.body.instructions &&
+    typeof(req.body.instructions) === 'string' &&
+    req.body.step_number &&
+    typeof(req.body.step_number) === 'number' &&
+    req.body.step_number > 0
+  ) {
+    next();
+  } else {
+    next({status: 400, message: 'invalid step'});
+  }
 }
 
 module.exports = {
--- api/schemes/scheme-router.js
+++ api/schemes/scheme-router.js
@@ -5,82 +5,24 @@ const Schemes = require('./scheme-model.js')
 
 const router = express.Router()
 
-/**
-  [GET] /api/schemes
-
-  response:
-  [
-    {
-      "scheme_id": 1,
-      "scheme_name": "World Domination",
-      "number_of_steps": 3
-    },
-    {
-      "scheme_id": 2,
-      "scheme_name": "Get Rich Quick",
-      "number_of_steps": 2
-    },
-    // etc
-  ]
- */
 router.get('/', (req, res, next) => {
   Schemes.find()
     .then(schemes => {
-      res.json(schemes)
+      res.json(schemes);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
-/*
-  [GET] /api/schemes/2
-
-  response:
-  {
-    "scheme_id": 2,
-    "scheme_name": "Get Rich Quick",
-    "steps": [
-      {
-          "step_id": 5,
-          "step_number": 1,
-          "instructions": "collect all the sheep in Scotland"
-      },
-      {
-          "step_id": 4,
-          "step_number": 2,
-          "instructions": "profit"
-      }
-    ]
-  }
-*/
 router.get('/:scheme_id', checkSchemeId, (req, res, next) => {
   const { scheme_id } = req.params
 
   Schemes.findById(scheme_id)
     .then(scheme => {
-      res.json(scheme)
+      res.json(scheme);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
-/*
-  [GET] /api/schemes/2/steps
-
-  response:
-  [
-    {
-      "step_id": 5,
-      "step_number": 1,
-      "instructions": "collect all the sheep in Scotland",
-      "scheme_name": "Get Rich Quick"
-    },
-    {
-      "step_id": 4,
-      "step_number": 2,
-      "instructions": "profit",
-      "scheme_name": "Get Rich Quick"
-    }
-  ]
-*/
 router.get('/:scheme_id/steps', checkSchemeId, (req, res, next) => {
   const { scheme_id } = req.params
 
@@ -100,6 +42,7 @@ router.get('/:scheme_id/steps', checkSchemeId, (req, res, next) => {
     "scheme_name": "Take Ovah"
   }
 */
+
 router.post('/', validateScheme, (req, res, next) => {
   const scheme = req.body
 
